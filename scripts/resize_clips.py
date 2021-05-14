881f9b443094136cc839d8f54f700a9e10cdc7fb
Paolo Mandica <paolomandica.p@gmail.com>
2021-05-14 17:15:57 +0200

    Add multiprocessing

--- scripts/resize_clips.py
+++ scripts/resize_clips.py
@@ -5,12 +5,34 @@ from pathlib import Path
 from tqdm import tqdm
 from time import time
 from datetime import timedelta
+from multiprocessing import Pool
+from functools import partial
+from multiprocessing import current_process
 
 
 def resize_clip(input_path, output_path, size=256, logger="bar", threads=6):
     clip = mp.VideoFileClip(input_path, audio=False)
     clip_resized = clip.resize(newsize=(size, size))
     clip_resized.write_videofile(output_path, logger=logger, threads=threads)
+
+
+def resize_dir(input_dir, output_dir, size, subdir):
+    print("===== Resizing clips in %s" %
+          (os.path.join(input_dir, subdir)))
+    clips = os.listdir(os.path.join(input_dir, subdir))
+    current = current_process()
+    pos = current._identity[0]-1 if len(current._identity) > 0 else None
+    for clip in tqdm(clips, position=pos):
+        input_path = os.path.join(input_dir, subdir, clip)
+        output_path = os.path.join(output_dir, subdir, clip)
+        if not os.path.exists(output_path):
+            try:
+                resize_clip(input_path, output_path, size, None)
+            except:
+                print("\nFailed: %s" % (output_path))
+                with open(os.path.join(output_dir, "failed_clips.txt"), "a") as txt:
+                    txt.write(output_path + "\n")
+    print("\n")
 
 
 def makedirs(dir1, dir2):
@@ -20,31 +42,27 @@ def makedirs(dir1, dir2):
     return subdirs
 
 
-def resize_multiple_clips(input_dir, output_dir, size=256):
-    subdirs = makedirs(input_dir, output_dir)
+def resize_multiple_clips(input_dir, output_dir, size=256, workers=1):
 
     print("\n=========== Starting resizing ===========\n")
 
     start = time()
-    tot_dirs = len(subdirs)
-    for subdir in subdirs:
-        print("%d folders left\n" % (tot_dirs))
-        tot_dirs -= 1
 
-        print("===== Resizing clips in %s" %
-              (os.path.join(input_dir, subdir)))
-        clips = os.listdir(os.path.join(input_dir, subdir))
-        for clip in tqdm(clips):
-            input_path = os.path.join(input_dir, subdir, clip)
-            output_path = os.path.join(output_dir, subdir, clip)
-            if not os.path.exists(output_path):
-                try:
-                    resize_clip(input_path, output_path, size, None)
-                except:
-                    print("\nFailed: %s" % (output_path))
-                    with open(os.path.join(output_dir, "failed_clips.txt"), "a") as txt:
-                        txt.write(output_path + "\n")
-        print("\n")
+    subdirs = makedirs(input_dir, output_dir)
+    tot_dirs = len(subdirs)
+
+    if workers > 1:
+        n_workers = workers if tot_dirs > workers else tot_dirs
+        func = partial(resize_dir, input_dir, output_dir, size)
+
+        with Pool(n_workers) as pool:
+            pool.map(func, subdirs)
+
+    else:
+        for subdir in subdirs:
+            print("%d folders left\n" % (tot_dirs))
+            tot_dirs -= 1
+            resize_dir(input_dir, output_dir, size, subdir)
 
     end = time()
     tot_time = str(timedelta(seconds=round(end-start)))
@@ -53,11 +71,11 @@ def resize_multiple_clips(input_dir, output_dir, size=256):
 
 if __name__ == "__main__":
     # input_path is the path to the folder containing the subfolders (applauding, jogging, ...) with the clips
-    input_path = "/home/paolo/dev/data_science/th_proj/videowalk/data/kinetics400/"
+    input_path = "/home/paolo/dev/data_science/th_proj/kinetics-downloader/dataset/train/"
     # the output_path is the path to the output folder where the new subfolders will be stored
     # if the output folder doesn't exists it will be automatically created with all the parent folders
-    output_path = "/home/paolo/dev/data_science/th_proj/videowalk/data/kinetics400_resized/"
+    output_path = "/home/paolo/dev/data_science/th_proj/kinetics400/train_256/"
 
     Path(output_path).mkdir(parents=True, exist_ok=True)
 
-    resize_multiple_clips(input_path, output_path)
+    resize_multiple_clips(input_path, output_path, workers=1)
