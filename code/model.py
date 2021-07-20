7b0de2214a084872ae0d91bd7795ed4d1fe73835
paolomandica <paolomandica.p@gmail.com>
2021-07-20 18:20:58 +0200

    Minor changes

--- code/model.py
+++ code/model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision
 
 import numpy as np
 import utils
@@ -38,8 +37,13 @@ class CRW(nn.Module):
         dummy = torch.zeros(1, 3, 1, in_sz, in_sz).to(
             next(self.encoder.parameters()).device)
         dummy_out = self.encoder(dummy)
+        print("Dummy output ====>", dummy_out.shape)
         self.enc_hid_dim = dummy_out.shape[1]
         self.map_scale = in_sz // dummy_out.shape[-1]
+        out = self.encoder(torch.zeros(1, 3, 1, 320, 320).to(
+            next(self.encoder.parameters()).device))
+        print("OUT SHAPE:", out.shape)
+        # scale = out[1].shape[-2:]
 
     def make_head(self, depth=1):
         head = []
@@ -116,6 +120,8 @@ class CRW(nn.Module):
         feats = feats.view(B, N, feats.shape[1], T).permute(0, 2, 3, 1)
         maps = maps.view(B, N, *maps.shape[1:])
 
+        print("FINAL FEATS", feats.shape)
+        print("FINAL MAPS", maps.shape)
         return feats, maps
 
     def forward(self, x, just_feats=False,):
