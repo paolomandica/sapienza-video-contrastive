3ade459e7ddc973bc17fe5e50c9f680c3f574d32
anilkeshwani <anilkeshwani@hotmail.com>
2021-11-09 15:11:19 +0000

    Update test.sh

--- code/test.sh
+++ code/test.sh
@@ -1,18 +1,32 @@
+# Checkpoint to Evaluate
+checkpoint="./checkpoints/parallel_latent_superpixels_0/model_2.pth"
+
+# Delete Contents of Results Directory
+# rm -rf ../results/*
+
+# Results Output Directories
+savepath="../results/masks/"
+outpath="../results/converted/"
+
+# Davis Paths
 vallist="/data_volume/sapienza-video-contrastive/code/eval/davis_vallist.txt"
-checkpoint="/data_volume/sapienza-video-contrastive/code/checkpoints/_drop0.1-len4-ftranscrop-fauggrid-optimadam-temp0.05-fdrop0.0-lr0.0001-mlp0-spslic-nsp30-p0.7/model_1.pth"
-savepath="../results/latent_slic_1_run2/"
-outpath="../results/latent_slic_1_run2_converted/"
 dataset="/data_volume/data/davis_val/"
 
+# # Pretrained Model Paths
+# pretrained_checkpoint="../pretrained.pth"
+# pretrained_savepath="../results/pretrained/"
+# pretrained_outpath="../results/pretrained_converted/"
+
+# Run Evaluation
 python test.py --filelist $vallist --model-type scratch \
 --resume $checkpoint --save-path $savepath \
---topk 10 --videoLen 20 --radius 12  --temperature 0.05  --cropSize -1
+--topk 10 --videoLen 20 --radius 12  --temperature 0.05  --cropSize -1 
 
-### Convert
+# Convert
 python ./eval/convert_davis.py --in_folder $savepath \
 --out_folder $outpath --dataset $dataset
 
-### Compute metrics
+# Compute metrics
 python /data_volume/davis2017-evaluation/evaluation_method.py \
 --task semi-supervised  --results_path $outpath \
 --set val --davis_path $dataset
